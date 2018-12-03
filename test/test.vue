c3a58a2fff96342a416502265898d6f5e13b0c60
Diskobólos <ehazomm@gmail.com>
2018-12-03 19:18:50 +0800

    Update test.vue

--- test/test.vue
+++ test/test.vue
@@ -11,12 +11,12 @@
     name: "test",
     mounted() {
       const dragActive = new DragActive({
-        offset: 20,
-        el: this.$refs.button,
+        offset: 20, // 针对屏幕边缘的距离
+        el: this.$refs.button, // 需要标记可拖拽的元素
         coordinateX: 100, // 初始化坐标
         coordinateY: 200, // 初始化坐标
         coordinateSave: true, // 自动保存拖拽位置
-        // transition: '',
+        // transition: '', // 尚未启用，预留用于拖拽时的动画渐变
       })
 
       dragActive.run({
